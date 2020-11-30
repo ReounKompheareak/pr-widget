5bd2297c73c91d6e0ffbce3021d1d2fbbc433fce
Kompheareak <reounkompheareak@gmail.com>
2020-11-30 10:42:45 +0700

    pagination

--- lib/widget/pagination_widget.dart
+++ lib/widget/pagination_widget.dart
@@ -1,69 +1,86 @@
 import 'package:flutter/material.dart';
 
 class PaginationWidget<T> extends StatefulWidget {
-
   bool shrinkWrap;
   T datas;
   Widget Function(dynamic data) child;
   ScrollController controller;
-  int initPage;
-  Future<T> Function(int) fetchData;
-  
+  Future<T> Function(int) onApiRequest;
 
-  PaginationWidget({@required this.controller,
-                    @required this.child,
-                    @required this.datas,
-                    @required this.fetchData,
-                    this.initPage = 1,
-                    this.shrinkWrap=false});
+  PaginationWidget(
+      {@required this.controller,
+      @required this.child,
+      @required this.datas,
+      @required this.onApiRequest,
+      this.shrinkWrap = false});
 
   @override
   _PaginationWidgetState createState() => _PaginationWidgetState();
 }
 
 class _PaginationWidgetState<T> extends State<PaginationWidget> {
-  int page ;
+  int page = 1;
   bool isLoading = false;
   var data = [];
-
 
   @override
   void initState() {
     data = widget.datas;
-    page = widget.initPage;
+    print("Called initState");
     widget.controller.addListener(() {
-      if(widget.controller.position.maxScrollExtent == widget.controller.position.pixels){
+      if (widget.controller.position.maxScrollExtent ==
+          widget.controller.position.pixels) {
         page++;
         setState(() {
           isLoading = true;
         });
-        widget.fetchData.call(page).whenComplete((){
+        widget.onApiRequest.call(page).then((value) {
+          setState(() {
+            isLoading = false;
+          });
+          // data+=value;    
+          
+          data = data + value;
+        }).catchError((e) {
           setState(() {
             isLoading = false;
           });
         });
       }
-     });
+    });
   }
-
-
 
   @override
   Widget build(BuildContext context) {
-    data += widget.datas;
+    // for(var i in widget.datas){
+    //       print("FetchData: ${i.firstname}");
+    //     }
     return Stack(
       children: [
         ListView(
-          shrinkWrap: widget.shrinkWrap,
-          controller: widget.controller,
-          children: data.map((e) => widget.child(e)).toList(),
-        ),
-        isLoading ? Positioned(
-          bottom: 1,
-          left: MediaQuery.of(context).size.width*0.9,
-          right: MediaQuery.of(context).size.width*0.9,
-          child: CircularProgressIndicator(),
-        ): Container()
+            shrinkWrap: widget.shrinkWrap,
+            controller: widget.controller,
+            children: data.map((e) => widget.child(e)).toList()),
+        isLoading
+            ? Positioned(
+                bottom: 15,
+                left: 1,
+                right: 1,
+                child: Center(
+                    child: Container(
+                        width: 35,
+                        height: 35,
+                        child: Card(
+                            shape: RoundedRectangleBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(64))),
+                            elevation: 2,
+                            child: Padding(
+                              padding: const EdgeInsets.all(4.0),
+                              child: CircularProgressIndicator(strokeWidth: 3),
+                            )))),
+              )
+            : Container()
       ],
     );
   }
